45916b98f2faa055483527b422e5b294d99bd725
RizqiAnshari <muhammadrizqia@student.telkomuniversity.ac.id>
2022-03-04 14:54:09 +0800

    tambahan fitur gui

--- TP-Mod2/TP-Mod2/Form1.Designer.cs
+++ TP-Mod2/TP-Mod2/Form1.Designer.cs
@@ -28,20 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.submit = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // submit
+            // 
+            this.submit.Location = new System.Drawing.Point(272, 244);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(75, 23);
+            this.submit.TabIndex = 0;
+            this.submit.Text = "Submit";
+            this.submit.UseVisualStyleBackColor = true;
+            this.submit.Click += new System.EventHandler(this.submit_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(272, 218);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(75, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(272, 276);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 22);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Output";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(617, 323);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.submit);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button submit;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- TP-Mod2/TP-Mod2/Form1.cs
+++ TP-Mod2/TP-Mod2/Form1.cs
@@ -16,5 +16,36 @@ namespace TP_Mod2
         {
             InitializeComponent();
         }
+
+        //private void label1_click(object sender, eventargs e)
+        //{
+
+        //}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // ngambil nama terus kirim ke label ketika submit ditekan
+            string var;
+            var = label1.Text;
+        }
+
+        private void submit_Click(object sender, EventArgs e)
+        {
+            //label1.Text = "Hello World!";
+            label1.Text = textBox1.Text;
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+            // mendapatkan nama dari textbox
+
+        }
+
+        
     }
 }
